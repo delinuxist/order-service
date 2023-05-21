f6421765dcaf8b7c2eafc3533b4208015d937f63
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-05-21 18:34:14 +0000

    TP-44 portfolio crud operations

---
+++ src/main/java/com/tradingengine/orderservice/exception/portfolio/PortfolioNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tradingengine.orderservice.exception.portfolio;
+
+public class PortfolioNotFoundException extends Exception{
+    public PortfolioNotFoundException(String message) {
+        super(message);
+    }
+}
